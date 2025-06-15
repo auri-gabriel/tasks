a0df94da696db8756cf399bcf9123f408bea8022
Auri Gabriel <ririmello.a@gmail.com>
2025-06-15 00:50:36 -0300

    feat: add Task entities

---
+++ src/main/java/com/aurigabriel/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.aurigabriel.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ src/main/java/com/aurigabriel/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.aurigabriel.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
