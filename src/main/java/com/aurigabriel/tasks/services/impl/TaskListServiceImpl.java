1a2c2df50c51bfbedcd46f4b3be96f1ce0562c19
Auri Gabriel <ririmello.a@gmail.com>
2025-06-16 10:05:01 -0300

    feat: add createTaskList method to TaskListService and implement it in TaskListServiceImpl

--- src/main/java/com/aurigabriel/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/aurigabriel/tasks/services/impl/TaskListServiceImpl.java
@@ -5,6 +5,7 @@ import com.aurigabriel.tasks.repositories.TaskListRepository;
 import com.aurigabriel.tasks.services.TaskListService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,5 +20,30 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> listTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if (null != taskList.getId()) {
+            throw new IllegalArgumentException("TaskList already has an ID!");
+        }
+
+        if(null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+            throw new IllegalArgumentException("TaskList title is required!");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+
+        ));
+
+        return null;
     }
 }
