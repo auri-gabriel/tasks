66ad46bb32d67366d5a4fdad90ad57f08ba63a2e
Auri Gabriel <ririmello.a@gmail.com>
2025-06-16 12:17:53 -0300

    feat: add getTaskList endpoint

--- src/main/java/com/aurigabriel/tasks/services/TaskListService.java
+++ src/main/java/com/aurigabriel/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.aurigabriel.tasks.services;
 import com.aurigabriel.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
